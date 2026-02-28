3fc10271aeca8d4c75c72f17e421e8aa5252b94a
MarediaX <darkdatadrop@gmail.com>
2026-02-28 01:14:12 -0500

    Fix dashboard and restore intelligence reports

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -2,6 +2,10 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/assets");
 
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/posts/*.md");
+  });
+
   return {
     dir: {
       input: "src",
